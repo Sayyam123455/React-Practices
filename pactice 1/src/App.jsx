434ae1850b4fc49143191752aa7ba6c38e19986f
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-06-11 22:28:39 +0530

    Initial commit

---
+++ pactice 1/src/App.jsx
@@ -0,0 +1,27 @@
+function App(){
+  return(
+<div>
+    <h1>Sayyam</h1>
+    <Fruit />
+    <Color />
+    </div>
+  )
+}
+
+export default App
+
+function Fruit(){
+  return (
+    <div>
+    <h1>Apple</h1>
+    
+    </div>
+  )
+}
+
+
+function Color(){
+  return (
+    <h1>Red</h1>
+  )
+}
