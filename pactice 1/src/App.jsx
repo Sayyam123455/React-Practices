319497d786503103aa007d21ef2ecd6c63a33f8d
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-06-14 15:53:01 +0530

    functions control in react

--- pactice 1/src/App.jsx
+++ pactice 1/src/App.jsx
@@ -1,41 +1,20 @@
-import Todo from "./ToDo"
+ const fruit = (name)=>{
+  alert(name);
+ }
 
 
 function App(){
-  const name ="Sayyam";
-  const x= 10;
-  const y= 20;
- const ObjectSam={
-    name: "Sayyam",
-    email: "sam@gmail.com",
-age:"29"
-  }
-  const img="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"
-  const arr=[8,9,5,4,5]
-  function data(){
-    return "fruit";
-  }
-  function marks(a, b, op){
-    if(op=="+"){
-      return a+b;
-    }
-    else if(op=="-"){
-      return a-b;
-    }
-else{
-      return a*b;
-}    
-  }
+ function name(){
+alert("name Submitted")
+ }
 
+//  const fruit = (name)=>{
+//   alert(name);
+//  }
   return(
 <div>
-    <h1>{name?name:"User not Found"}</h1>
-    <h1>{x+y}</h1>
-    <h1>{data()}</h1>
-    <h1>{marks(4,6, "")}</h1>
-    <h1>{ObjectSam.age}</h1>
-    <h1>{arr[3]}</h1>
-<img src={img}/>
+   <button onClick={()=>fruit("apple")}>Apple</button>
+   <button onClick={()=>fruit("banana")}>Banana</button>
     </div>
   )
 }
