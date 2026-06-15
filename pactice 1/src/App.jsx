44071982ff946722f84aa030c2bed918c61cb50e
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-06-15 22:29:26 +0530

    multiple condition

--- pactice 1/src/App.jsx
+++ pactice 1/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react"
-import Counter from "./counter"
+
 
 function App(){
-const[display, setDisplay]=useState(true)
+  const [count, setCount]=useState(0);
   return(
 <div>
-  <button onClick={()=>setDisplay(!display)}>Click Me</button>
-{
-  display?<Counter/>:null
-}
+  <h1>{count}</h1>
+  <button onClick={()=>setCount(count+1)}>Click me</button>
+  {
+    count==0?<h1>Coundition 0</h1>
+    :count==1?<h1>Coundition 1</h1>
+    :count==2?<h1>Coundition 2</h1>
+    :count==3?<h1>Coundition 3</h1>
+    :<h1>Other Condition</h1>
+
+  }
     </div>
   )
 }
