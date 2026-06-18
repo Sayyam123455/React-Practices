67d116d639c5fb7ae9776123ba648a0c0373ddfa
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-06-18 22:25:25 +0530

    controlled component

--- pactice 1/src/App.jsx
+++ pactice 1/src/App.jsx
@@ -9,11 +9,18 @@ function App(){
 // }
 // const arr=["data", "dsd","sjdgjsa"]
 const[data, setdata]=useState();
+const[password, setpassword]=useState();
+const[email, setemail]=useState();
   return(
 <div>
- <input type="text" value={data} onChange={(event)=>setdata(event.target.value)} />
+ <input type="text" value={data} onChange={(event)=>setdata(event.target.value)} /><br/><br/>
+ <input type="text" value={password} onChange={(event)=>setpassword(event.target.value)} /><br/><br/>
+ <input type="text" value={email} onChange={(event)=>setemail(event.target.value)} /><br/><br/>
+ 
+ <button onClick={()=>{setdata("");setpassword("");setemail("")}}>Click me</button>
  <h1>{data}</h1>
- <button onClick={()=>setdata("")}>Click me</button>
+ <h1>{password}</h1>
+ <h1>{email}</h1>
     </div>
   )
 }
