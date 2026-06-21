1a53c6eeff84cf1aba36bf3951bc0f7e5f71b597
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-06-21 20:20:12 +0530

    components transfer with prop

--- pactice 1/src/App.jsx
+++ pactice 1/src/App.jsx
@@ -4,10 +4,32 @@ import Skills from "./Skills"
 
 
 function App(){
-
+ const dataStore=[
+    {
+        Name:"Sayyam",
+        Age:29,
+        Email:"sayyam@test.com",
+        id:1
+    },
+    {
+        Name:"ram",
+        Age:20,
+        Email:"ram@test.com",
+        id:2
+    },
+    {
+        Name:"sham",
+        Age:25,
+        Email:"sham@test.com",
+        id:3
+    }
+ ]
   return(
-<div>
-<Skills />
+<div>{
+  dataStore.map((user)=>(
+  <Skills user={user}/>
+  ))
+}
     </div>
   )
 }
--- pactice 1/src/Skills.jsx
+++ pactice 1/src/Skills.jsx
@@ -1,53 +1,16 @@
 import { useState } from "react"
 
-function Skills(){
- const dataStore=[
-    {
-        Name:"Sayyam",
-        Age:29,
-        Email:"sayyam@test.com",
-        id:1
-    },
-    {
-        Name:"ram",
-        Age:20,
-        Email:"ram@test.com",
-        id:2
-    },
-    {
-        Name:"sham",
-        Age:25,
-        Email:"sham@test.com",
-        id:3
-    }
- ]
+function Skills({user}){
+
 
     return(
-        <>
-       <table border="1">
-        <thead>
-            <tr>
-                <td>Id</td>
-                <td>Name</td>
-                <td>Age</td>
-                <td>Email</td>
-            </tr>
-            </thead>
-            <tbody>
-                {
-                    dataStore.map((user)=>(
-                        <tr>
-                        <td>{user.id}</td>
-                        <td>{user.Name}</td>
-                        <td>{user.Age}</td>
-                        <td>{user.Email}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        
-       </table>
-                </>
+        <div className={user.id}>
+       
+            <h3>{user.Name}</h3>
+            <h3>{user.Age}</h3>
+            <h3>{user.id}</h3>
+            <h3>{user.Email}</h3>
+                </div>
     )
 
 }
