cd4997dbe84f2e3896d1b3eec81f5f9ce3cd7c40
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-06-21 11:39:44 +0530

    loop in jsx handle

--- pactice 1/src/Skills.jsx
+++ pactice 1/src/Skills.jsx
@@ -1,25 +1,52 @@
 import { useState } from "react"
 
 function Skills(){
-   const[gender, setGender]=useState("female");
-   const[city, setCity]=useState("noida");
+ const dataStore=[
+    {
+        Name:"Sayyam",
+        Age:29,
+        Email:"sayyam@test.com",
+        id:1
+    },
+    {
+        Name:"ram",
+        Age:20,
+        Email:"ram@test.com",
+        id:2
+    },
+    {
+        Name:"sham",
+        Age:25,
+        Email:"sham@test.com",
+        id:3
+    }
+ ]
 
     return(
         <>
-        <input type="radio" onChange={(event)=>setGender(event.target.value)} value={"male"} name="gender" checked={gender == "male"} id="male"/>
-        <label htmlFor="male">Male</label>
-        <input type="radio"  onChange={(event)=>setGender(event.target.value)} value={"female"} name="gender" checked={gender == "female"} id="female"/>
-        <label htmlFor="female">Female</label>
-        <h5>Gender Name: {gender}</h5>
-        <br />
-        <br />
-        <br />
-        <select name="city" onChange={(event)=>setCity(event.target.value)} id="city" defaultValue="gurgram">
-            <option value="delhi">delhi</option>
-            <option value="noida">noida</option>
-            <option value="gurgram">gurgram</option>
-        </select>
-        <h5>Selected City: {city}</h5>
+       <table border="1">
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Age</td>
+                <td>Email</td>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    dataStore.map((user)=>(
+                        <tr>
+                        <td>{user.id}</td>
+                        <td>{user.Name}</td>
+                        <td>{user.Age}</td>
+                        <td>{user.Email}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        
+       </table>
                 </>
     )
 
