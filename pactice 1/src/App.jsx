08815d365f48ea6bc60f3fe8f7e6b244035ed745
Sayyam Kukreja <sayyamkukreja456@gmail.com>
2026-07-05 17:57:26 +0530

    useref hooks

--- pactice 1/src/App.jsx
+++ pactice 1/src/App.jsx
@@ -1,17 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRef } from "react";
 
-import { Alert, Button } from 'react-bootstrap';
 
 function App() {
-
-
+const inputref=useRef(null)
+function placeholder(){
+  inputref.current.placeholder="Password"
+}
+function toggle(){
+  if( inputref.current.style.display!="none"){
+  inputref.current.style.display="none"}
+  else{
+    inputref.current.style.display="inline"
+  }
+}
   return (
 <>
- <Alert variant="danger">
-          This is a red alert—check it out!
-        </Alert>
-        <Button variant="info">Info</Button>
-      <Button variant="light">Light</Button>
+<button onClick={toggle}>toggle</button>
+
+<input ref={inputref} type="text" placeholder="enter your name" />
+<button onClick={placeholder}>Change</button>
 </>
 
   );
